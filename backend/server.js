b9204a2b11d7fc06d907c413f361b3c98798d8b8
Timur <m2205151@edu.misis.ru>
2025-02-24 18:05:54 +0300

    token bucket

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import helmet from "helmet"
 import morgan from "morgan";
 import cors from "cors"
@@ -6,6 +6,7 @@ import dotenv from "dotenv"
 
 import productRoutes from "./routes/productRoutes.js"
 import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
 
 
 dotenv.config()
@@ -17,6 +18,31 @@ app.use(express.json())
 app.use(cors())
 app.use(helmet());
 app.use(morgan("dev"))
+
+app.use(async (req,res,next)=>{
+    try {
+        const decision = await aj.protect(req, {
+            requested: 1
+        })
+
+        if (decision.isDenied()) {
+            if (decision.reason.isRateLimit()) {
+                res.status(429).json({error:" Too many requests "})
+            } else if (decision.reason.isBot()) {
+                res.status(403).json({error:" Bot access denied "})
+            } else {
+                res.status(403).json({error:" Forbidden "})
+            }
+            return
+        }
+
+        next()
+
+    } catch (error) {
+        console.log("Arcjet error", error)
+        next(error)
+    }
+})
 
 app.use("/api/products", productRoutes)
 
