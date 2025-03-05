60a4228e9b81a5a91bef9351db62f0b2eb5721fb
Timur <m2205151@edu.misis.ru>
2025-03-05 13:26:20 +0300

    ProductPage

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -13,6 +13,8 @@ export const useProductStore = create((set, get) => ({
   loading: false,
   error:null,
 
+  currentProduct: null,
+
 
   formData: {
     name: "",
@@ -71,6 +73,36 @@ export const useProductStore = create((set, get) => ({
     } finally {
       set({loading: false});
     }
-  }
+  },
+
+
+  fetchProduct: async (id) => {
+    set({loading: true});
+    try {
+      const response = await axios.get(`${BASE_URL}/api/products/${id}`);
+      set({currentProduct: response.data.data, formData: response.data.data, error: null});
+    } catch (error) {
+      console.log("Error in fetchProduct function", error);
+      set({error: "Something went wrong", currentProduct: null});
+    } finally {
+      set({loading: false})
+    }
+  },
+
+
+  updateProduct: async (id) => {
+    set({loading: true});
+    try {
+      const {formData} = get();
+      const response = await axios.put(`${BASE_URL}/api/products/${id}`, formData);
+      set({currentProduct: response.data.data, error: null});
+      toast.success("Product updated successfully");
+    } catch (error) {
+      toast.error("Something went wrong");
+      console.log("Error in updateProduct function", error);
+    } finally {
+      set({loading: false});
+    }
+  },
 }));
 
